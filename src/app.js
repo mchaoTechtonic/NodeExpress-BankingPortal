f907a6dcbbbc7c58acda89c928018757fc82a279
Matthew Chao <matthew.chao@techtonic.com>
2020-10-28 10:18:09 -0400

    use read file functions to pass failing tests 3.1 and 3.2; run prettier

--- src/app.js
+++ src/app.js
@@ -11,12 +11,23 @@ app.set("view engine", "ejs");
 // Task 2.4
 app.use(express.static(path.join(__dirname, "public")));
 
-// Task 2.7
+//Task 3.1+3.2
+const accountData = fs.readFileSync(
+  path.join(__dirname, "json", "accounts.json"),
+  { encoding: "utf8" }
+);
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(path.join(__dirname, "json", "users.json"), {
+  encoding: "utf8",
+});
+const users = JSON.parse(userData);
+
+// Task 2.7 -- the index route
 app.get("/", function (req, res) {
   res.render("index", { title: "Index" });
 });
 
 // Task 2.8
-app.listen(3000,()=>{
-    console.log("PS Project Running on port 3000!");
+app.listen(3000, () => {
+  console.log("PS Project Running on port 3000!");
 });
